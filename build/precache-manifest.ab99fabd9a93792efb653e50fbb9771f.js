15cd7e5c00936f0f1192dae5ac0fe2559ee88ad2
Ustimenko Denis <ustimenkodenis@mail.ru>
2020-06-13 16:11:21 +0300

    heroku added

---
+++ build/precache-manifest.ab99fabd9a93792efb653e50fbb9771f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daea93416951526ca224f8276c096a24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b093280553184bcb7ea",
+    "url": "/static/css/main.521969a7.chunk.css"
+  },
+  {
+    "revision": "e6345b28742271371de7",
+    "url": "/static/js/2.5afd95fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5afd95fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b093280553184bcb7ea",
+    "url": "/static/js/main.cfd036fa.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
